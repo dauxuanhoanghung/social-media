63a2d0e4f54e469a65a233ff40675465d86e90f0
dauxuanhoanghung <dxhh2510@gmail.com>
2023-09-09 23:58:30 +0700

    stats end

--- SocialMediaBackend/src/main/java/com/social/repositories/impl/StatsRepositoryImpl.java
+++ SocialMediaBackend/src/main/java/com/social/repositories/impl/StatsRepositoryImpl.java
@@ -36,35 +36,35 @@ import org.springframework.transaction.annotation.Transactional;
 @Repository
 @Transactional
 public class StatsRepositoryImpl implements StatsRepository {
-
+    
     @Autowired
     private LocalSessionFactoryBean sessionFactory;
-
+    
     @Autowired
     private DateTimeFormatter dateTimeFormatter;
-
+    
     private Session getSession() {
         SessionFactory sessionFactory = this.sessionFactory.getObject();
         return sessionFactory.getCurrentSession();
     }
-
+    
     @Override
     public List<Object[]> countNewUsers(Map<String, String> params) {
         Session session = getSession();
-
+        
         CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
         CriteriaQuery<Object[]> criteriaQuery = criteriaBuilder.createQuery(Object[].class);
-
+        
         Root<User> userRoot = criteriaQuery.from(User.class);
         Join<Role, User> roleJoin = userRoot.join("role", JoinType.LEFT);
-
+        
         criteriaQuery.multiselect(
                 roleJoin.get("id"),
                 roleJoin.get("name"),
                 criteriaBuilder.count(userRoot.get("id"))
         );
         List<Predicate> predicates = new ArrayList<>();
-
+        
         if (params != null) {
             String fromDate = params.get("fromDate");
             if (fromDate != null && !fromDate.isEmpty()) {
@@ -76,29 +76,29 @@ public class StatsRepositoryImpl implements StatsRepository {
                 predicates.add(criteriaBuilder.lessThanOrEqualTo(userRoot.get("createdDate"),
                         LocalDateTime.parse(toDate, dateTimeFormatter)));
             }
-
+            
             String roleId = params.get("roleId");
             if (roleId != null && !roleId.isEmpty()) {
                 predicates.add(criteriaBuilder.equal(userRoot.get("role"),
                         Integer.valueOf(roleId)));
             }
-
+            
             criteriaQuery.where(predicates.toArray(Predicate[]::new));
         }
         criteriaQuery.groupBy(roleJoin.get("id"), roleJoin.get("name"));
         Query query = session.createQuery(criteriaQuery);
         return query.getResultList();
     }
-
+    
     @Override
     public List<Object[]> countPosts(Map<String, String> params) {
         Session session = getSession();
-
+        
         CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
         CriteriaQuery<Object[]> criteriaQuery = criteriaBuilder.createQuery(Object[].class);
         Root<Post> postRoot = criteriaQuery.from(Post.class);
         List<Predicate> predicates = new ArrayList<>();
-
+        
         if (params != null) {
             if (params.containsKey("fromDate")) {
                 predicates.add(criteriaBuilder.greaterThanOrEqualTo(postRoot.get("createdDate"),
@@ -108,14 +108,15 @@ public class StatsRepositoryImpl implements StatsRepository {
                 predicates.add(criteriaBuilder.lessThanOrEqualTo(postRoot.get("createdDate"),
                         LocalDateTime.parse(params.get("toDate"), dateTimeFormatter)));
             }
-
+            
             criteriaQuery.where(predicates.toArray(Predicate[]::new));
         }
-        criteriaQuery.groupBy();
+        criteriaQuery.multiselect(postRoot.get("type"), criteriaBuilder.count(postRoot.get("id")));
+        criteriaQuery.groupBy(postRoot.get("type"));
         Query query = session.createQuery(criteriaQuery);
         return query.getResultList();
     }
-
+    
     @Override
     public List<Object[]> top10MostActiveUser(Map<String, String> params) {
         Session session = getSession();
@@ -130,7 +131,7 @@ public class StatsRepositoryImpl implements StatsRepository {
         Join<User, Post> userJoinPost = userRoot.join("posts", JoinType.LEFT);
         Join<User, PostAction> userJoinPostAction = userRoot.join("postActions", JoinType.LEFT);
         Join<User, CommentAction> userJoinCommentAction = userRoot.join("commentActions", JoinType.LEFT);
-
+        
         criteriaQuery.multiselect(
                 userRoot,
                 criteriaBuilder.countDistinct(userJoinPost.get("id")).alias("post_count"),
@@ -185,5 +186,5 @@ public class StatsRepositoryImpl implements StatsRepository {
         List<Object[]> results = session.createQuery(criteriaQuery).getResultList();
         return results;
     }
-
+    
 }
